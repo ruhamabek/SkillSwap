1a2420e7f4c0a5a9339a0669b0b027293926cf8b
teshager habtie <teshie00001@gmail.com>
2025-03-28 12:45:39 -0500

    payment page

---
+++ client/src/pages/payment.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const payment = () => {
+  return (
+    <div>
+      check payment page
+      <h1 className="text-4xl font-bold mb-4">Payment</h1>
+    </div>
+  );
+}
+
+export default payment;
