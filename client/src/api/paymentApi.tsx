265427a4ccef781b72e9372088b37626f1ab1e2b
teshager habtie <teshie00001@gmail.com>
2025-03-30 13:50:55 -0500

    creating payment api

---
+++ client/src/api/paymentApi.tsx
@@ -0,0 +1,39 @@
+import { authClient } from "@/lib/auth-client";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+
+const API_BASE_URL = "http://localhost:3000/pay";
+
+interface PaymentVariables {
+  id: number;
+  amount: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+}
+
+const Payment = () => {
+  const session = authClient.useSession();
+  const queryClient = useQueryClient();
+
+  const paymentMutation = useMutation({
+    mutationFn: async (variables: PaymentVariables) => {
+      const { id, amount, email, first_name, last_name } = variables;
+      const response = await axios.post(
+        `${API_BASE_URL}/`,
+        { id, amount, email, first_name, last_name },
+        {
+          withCredentials: true,
+        }
+      );
+      return response.data.checkout_url;
+    },
+    onSuccess: () => {},
+  });
+
+  return {
+    paymentMutation,
+  };
+};
+
+export default Payment;
