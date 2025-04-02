0bc101d1fa8995e8c40a9e7226f0ca2b05338487
teshager habtie <teshie00001@gmail.com>
2025-04-02 12:34:23 -0500

    check

--- client/src/pages/payment.tsx
+++ client/src/pages/payment.tsx
@@ -1,3 +1,4 @@
+import { Link } from "lucide-react";
 import React from "react";
 
 const payment = () => {
@@ -5,8 +6,9 @@ const payment = () => {
     <div>
       check payment page
       <h1 className="text-4xl font-bold mb-4">Payment</h1>
+      <a href="/">backtohome</a>
     </div>
   );
-}
+};
 
 export default payment;
