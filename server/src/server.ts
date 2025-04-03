07a73fa9844ef66a0a373e4a64f86359e2fea5ee
teshager habtie <teshie00001@gmail.com>
2025-04-03 17:02:23 -0500

    import and use socket io

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,23 +1,31 @@
 import "dotenv/config";
 import express, { Request, Response } from "express";
 import cors from "cors";
-import { connect as authConnect} from "./db/mongo-client";
+import { connect as authConnect } from "./db/mongo-client";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
-import {connect as dbConnect} from "./db/mongoose"
+import { connect as dbConnect } from "./db/mongoose";
 import profileRoute from "../src/routes/ProfileRoute";
 import paymentRoute from "../src/routes/paymentRoute";
+import http from "http";
+import socketHandler from "./socket";
 
 const app = express();
+const server = http.createServer(app);
+
+// Initialize Socket.IO
+socketHandler(server);
+
+// const app = express();
 const PORT = 3000;
 
 app.use(
-    cors({
-      origin: "http://localhost:8080", 
-      methods: ["GET", "POST", "PUT", "DELETE"], 
-      credentials: true,
-    })
-  );
+  cors({
+    origin: "http://localhost:8080",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.all("/api/auth/*", toNodeHandler(auth));
 
 app.use(express.json());
@@ -26,14 +34,14 @@ app.get("/health", async (req: Request, res: Response) => {
   res.send({ message: "health OK!" });
 });
 
-app.use("/profile" , profileRoute);
+app.use("/profile", profileRoute);
 app.use("/pay", paymentRoute);
 
 async function startServer() {
   try {
     await authConnect();
     await dbConnect();
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`Server running at http://localhost:${PORT}`);
     });
   } catch (error) {
@@ -46,5 +54,3 @@ async function startServer() {
 }
 
 startServer();
-
-
